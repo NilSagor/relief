2b6b4a3f2676acd97cb5d46fb1c697014ae0db9c
NilSagor <masum1201@gmail.com>
2018-08-20 03:46:35 +0800

    request model

--- refegue/models.py
+++ refegue/models.py
@@ -31,6 +31,53 @@ class Request(models.Model):
 	needkit_util = models.BooleanField(verbose_name = 'Kitchen Utilities')
 	needrescue = models.BooleanField(verbose_name = 'Rescue')
 
+	detailwater = models.CharField(max_length = 250, verbose_name = 'Detail for required water', blank = True)
+	detailfood = models.CharField(max_length = 250, verbose_name = 'Detail for requird food', blank = True)
+	detailcloth = models.CharField(max_length = 250, verbose_name = 'Detail for requird cloth', blank = True)
+	detailmed = models.CharField(max_length = 250, verbose_name = 'Detail for required medicine', blank = True)
+	detailtoilet = models.CharField(max_length = 250, verbose_name = 'Detail for required toilet', blank = True)
+	detailkt_utils = models.CharField(max_length = 250, verbose_name = 'Detail for required kitchen utilities', blank = True)
+	detailrescue = models.CharField(max_length = 250, verbose_name = 'Detail for required rescue', blank = True)
+
+	needothers = models.CharField(max_length = 500, verbose_name = 'required others', blank = True)
+
+	status = models.CharField(
+		max_length = 10,
+		choices = status_type,
+		default = 'new'
+		)
+	supply_details = models.CharField(max_length = 10, blank = True)
+	dateadd = models.DateTimeField(auto_now_add = True)
+
+
+	def summarise(self):
+		out = " "
+
+		if (self.needwater):
+			out += "water requirements :\n {}".format(self.detailwater)
+		if (self.needfood):
+			out += "food requirements :\n {}".format(self.detailfood)
+		if (self.needcloth):
+			out += "cloth requirements :\n {}".format(self.detailcloth)
+		if (self.needmed):
+			out += "medicine requirements :\n {}".format(self.detailmed)
+		if (self.needtoilet):
+			out += "toilet requirements :\n {}".format(self.detailtoilet)
+		if (self.needkit_util):
+			out += "Kitchen Utilities requirements :\n {}".format(self.detailkt_utils)
+		if (self.needothers.strip() != 0):
+			out += "\nOthers needs :\n {}".format(self.needothers)
+		
+		return out
+
+	class Meta:
+		verbose_name = 'Rescue: Request'
+		verbose_name_request = 'Rescue : Resquest'
+
+	def __str__(self):
+		return self.get_district_display()+' '+self.location
+
+
 
 
 
